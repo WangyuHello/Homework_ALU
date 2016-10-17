c473efd6d26cc43800942fe0315b73207fac7f53
WangYu <wwangyuu@outlook.com>
2016-10-17 14:20:02 +0800

    finish look-ahead carry adder right part

--- Adder_Module_R_2.v
+++ Adder_Module_R_2.v
@@ -6,9 +6,8 @@ module Adder_SubModule_R2 (Ci_inverse,M_inverse,p,g,o);
 
   assign o[3] = ~(g[3]|(g[2]&p[3])|(g[1]&p[3]&p[2])|(g[0]&p[3]&p[2]&p[1]));
   assign o[2] = ~((p[2]&p[1]&p[0]&Ci_inverse&M_inverse)|(g[2]&M_inverse)|(p[2]&g[1]&M_inverse)|(p[2]&p[1]&g[0]&M_inverse));
-  assign o[1] = ~((p[1]&p[0]&Ci_inverse&M_inverse)|()|());
-
-
+  assign o[1] = ~((p[1]&p[0]&Ci_inverse&M_inverse)|(g[1]&M_inverse)|(p[1]&g[0]&M_inverse));
+  assign o[0] = ~((p[0]&Ci_inverse&M_inverse)|(g[0]&M_inverse));
 
 endmodule // Adder_SubModule_R2
 //Another part of adder
