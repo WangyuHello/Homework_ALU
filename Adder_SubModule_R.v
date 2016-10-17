fde878e92af08e1528b6ec6efa7c093b47554bd0
WangYu <wwangyuu@outlook.com>
2016-10-17 22:56:14 +0800

    test for git plus

--- Adder_SubModule_R.v
+++ Adder_SubModule_R.v
@@ -13,6 +13,9 @@ module Adder_SubModule_R (p,g,M,Ci_inverse,Co_inverse,P,Q,Y,AequalsB);
 
   assign P = ~(p[3]&p[2]&p[1]&p[1]);
 
-  Adder_SubModule_R2 adder_submodule_r2(.Ci_inverse(Ci_inverse));
+  Adder_SubModule_R2 adder_submodule_r2(.Ci_inverse(Ci_inverse),.M_inverse(M_inverse),.p(p),.g(g),.o(t));
+  Adder_SubModule_R1 r1();
+  Adder_SubModule_R1 r2();
+
 
 endmodule // Adder_SubModule_R
