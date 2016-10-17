1bd0bc74180fdc289b870579120d7bb79dffceb9
WangYu <wwangyuu@outlook.com>
2016-10-17 09:55:56 +0800

    update

--- Adder_SubModule_R.v
+++ Adder_SubModule_R.v
@@ -7,9 +7,11 @@ module Adder_SubModule_R (p,g,M,Ci,Co_inverse,P,Q,Y,AequalsB);
   output [3:0] Y;
 
   wire M_inverse = ~M;
+  wire t1,t2,t3,t4;
 
   assign P = ~(p[3]&p[2]&p[1]&p[1]);
 
-  
+  assign t1 = ~(g[3]|(g[2]&p[3])|(g[1]&p[3]&p[2])|(g[0]&p[3]&p[2]&p[1]));
+  assign t2 = 
 
 endmodule // Adder_SubModule_R
