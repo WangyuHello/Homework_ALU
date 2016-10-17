bd1fd7bd22565cc5f4065057fbaf28cf3cda112e
WangYu <wwangyuu@outlook.com>
2016-10-17 22:59:25 +0800

    update

--- Adder_SubModule_R.v
+++ Adder_SubModule_R.v
@@ -16,6 +16,6 @@ module Adder_SubModule_R (p,g,M,Ci_inverse,Co_inverse,P,Q,Y,AequalsB);
   Adder_SubModule_R2 adder_submodule_r2(.Ci_inverse(Ci_inverse),.M_inverse(M_inverse),.p(p),.g(g),.o(t));
   Adder_SubModule_R1 r1();
   Adder_SubModule_R1 r2();
-  
+
 
 endmodule // Adder_SubModule_R
