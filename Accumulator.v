b988ffc25733d95d6804e7257d78b70665d560c1
WangYu <wwangyuu@outlook.com>
2016-11-05 17:17:59 +0800

    Update Accumulator

--- Accumulator.v
+++ Accumulator.v
@@ -1,3 +1,13 @@
-module Accumulator ();
+module Accumulator (step,out);
+
+  input [3:0] step;
+
+  output [3:0] out;
+
+  wire Co_inverse,AequalsB;
+
+  reg [3:0] temp;
+
+  ALU alu(.a(step),.b(temp),.s(0110),.Ci_inverse(0),.P(0),.Q(0),.Co_inverse(Co_inverse),.Y(temp),.AequalsB(AequalsB));
 
 endmodule // Accumulator
