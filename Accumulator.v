0513b59d1c1214b2ab34330c5e1b99fd9c9fe798
WangYu <wwangyuu@outlook.com>
2016-11-07 13:28:26 +0800

    Add AccuTest

--- Accumulator.v
+++ Accumulator.v
@@ -1,13 +1,24 @@
-module Accumulator (step,out);
+module Accumulator (step,out,reset);
 
   input [3:0] step;
+  input reset;
 
   output [3:0] out;
 
   wire Co_inverse,AequalsB;
+  wire [3:0] Yo;
 
   reg [3:0] temp;
 
-  ALU alu(.a(step),.b(temp),.s(0110),.Ci_inverse(0),.P(0),.Q(0),.Co_inverse(Co_inverse),.Y(temp),.AequalsB(AequalsB));
+  ALU alu(.a(step),.b(temp),.s(0110),.Ci_inverse(0),.P(0),.Q(0),.Co_inverse(Co_inverse),.Y(Yo),.AequalsB(AequalsB));
+
+  temp[0] = Yo[0]&reset;
+  temp[1] = Yo[1]&reset;
+  temp[2] = Yo[2]&reset;
+  temp[3] = Yo[3]&reset;
+
+
+  out = temp;
+
 
 endmodule // Accumulator
