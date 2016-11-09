d1159f5a48d9a8e5c3db36e1befccdff80bc9893
WangYu <wwangyuu@outlook.com>
2016-11-09 22:02:09 +0800

    Correct Accumulator

--- Accumulator.v
+++ Accumulator.v
@@ -1,24 +1,36 @@
-module Accumulator (step,out,reset);
+`timescale 1ns / 10ps
+module Accumulator ();
+  reg [3:0] step,temp,mode;
+  reg Ci_inverse_in,M_in;
+  wire AequalsB_o,P_o,Q_o,Co_inverse_o;
+  wire [3:0] Y_o;
 
-  input [3:0] step;
-  input reset;
+  reg clk;
+  reg reset;
 
-  output [3:0] out;
+  ALU alu(.a(step),.b(temp),.s(mode),.M(M_in),.Ci_inverse(Ci_inverse_in),.P(P_o),.Q(Q_o),.Co_inverse(Co_inverse_o),.Y(Y_o),.AequalsB(AequalsB_o));
 
-  wire Co_inverse,AequalsB;
-  wire [3:0] Yo;
+  initial begin
+    step = 4'b0001;
+    temp = 4'b0000;
+    mode = 4'b1001;
+    M_in = 0;
+    Ci_inverse_in = 1;
 
-  reg [3:0] temp;
+    clk = 1;
+    reset = 0;
+    forever #100 clk = ~clk;
+    forever #1000 reset = ~reset;
+  end
 
-  ALU alu(.a(step),.b(temp),.s(0110),.Ci_inverse(0),.P(0),.Q(0),.Co_inverse(Co_inverse),.Y(Yo),.AequalsB(AequalsB));
+  always @ (posedge clk or reset) begin
 
-  temp[0] = Yo[0]&reset;
-  temp[1] = Yo[1]&reset;
-  temp[2] = Yo[2]&reset;
-  temp[3] = Yo[3]&reset;
+    temp[0] = Y_o[0]&(~reset);
+    temp[1] = Y_o[1]&(~reset);
+    temp[2] = Y_o[2]&(~reset);
+    temp[3] = Y_o[3]&(~reset);
 
-
-  out = temp;
+  end
 
 
 endmodule // Accumulator
